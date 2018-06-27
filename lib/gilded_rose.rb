7c6b7427c4b77d66e42f5c767e6916191df677ac
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2018-06-26 19:59:26 -0500

    add decrease_quality method with condition

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,22 +9,18 @@ class GildedRose
 
   def tick
     if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
-      if @quality > 0
-        if @name != "Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
+      decrease_quality
+    else
+      increase_quality
+      if @name == "Backstage passes to a TAFKAL80ETC concert"
+        if @days_remaining < 11
+          increase_quality
+        end
+
+        if @days_remaining < 6
+          increase_quality
         end
       end
-    else
-        increase_quality
-        if @name == "Backstage passes to a TAFKAL80ETC concert"
-          if @days_remaining < 11
-            increase_quality
-          end
-
-          if @days_remaining < 6
-            increase_quality
-          end
-        end
     end
     if @name != "Sulfuras, Hand of Ragnaros"
       @days_remaining = @days_remaining - 1
@@ -32,11 +28,7 @@ class GildedRose
     if @days_remaining < 0
       if @name != "Aged Brie"
         if @name != "Backstage passes to a TAFKAL80ETC concert"
-          if @quality > 0
-            if @name != "Sulfuras, Hand of Ragnaros"
-              @quality = @quality - 1
-            end
-          end
+          decrease_quality
         else
           @quality = @quality - @quality
         end
@@ -48,5 +40,11 @@ class GildedRose
 
   def increase_quality
     @quality += 1 if @quality < 50
+  end
+
+  def decrease_quality
+    if @quality > 0 && @name != "Sulfuras, Hand of Ragnaros"
+      @quality -= 1
+    end
   end
 end
